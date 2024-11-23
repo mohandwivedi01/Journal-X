79f20e1a9394cccfe43d302fbfa2247117e213d5
mohandwivedi01 <manmohandwivedi@yahoo.com>
2024-11-23 21:25:30 +0530

    fixed syntaxt errors

--- src/main/java/net/engineeringdigest/journalApp/model/JournalModel.java
+++ src/main/java/net/engineeringdigest/journalApp/model/JournalModel.java
@@ -2,17 +2,25 @@ package net.engineeringdigest.journalApp.model;
 
 import lombok.*;
 import org.bson.types.ObjectId;
+//import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+//import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+//import java.time.LocalDateTime;
 
 @Document(collection="journals")
 @Data
 @NoArgsConstructor
-
 public class JournalModel {
     @Id
     private ObjectId id;
     @NonNull
     private String title;
     private String content;
+//
+//    @CreatedDate
+//    private LocalDateTime createdAt;
+//    @LastModifiedDate
+//    private LocalDateTime updatedAt;
 }
--- src/main/java/net/engineeringdigest/journalApp/model/UserModel.java
+++ src/main/java/net/engineeringdigest/journalApp/model/UserModel.java
@@ -1,14 +1,15 @@
 package net.engineeringdigest.journalApp.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 import org.bson.types.ObjectId;
+//import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+//import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+//import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,4 +29,9 @@ public class UserModel {
     @DBRef
     private List<JournalModel>  journalEntries = new ArrayList<>();
     private List<String> roles;
+
+//    @CreatedDate
+//    private LocalDateTime createdAt;
+//    @LastModifiedDate
+//    private LocalDateTime updatedAt;
 }
