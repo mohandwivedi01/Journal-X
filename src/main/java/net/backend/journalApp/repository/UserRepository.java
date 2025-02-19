6c080864f1bc9da71da919e3616cd0e4cc86d80c
Mohan Dwivedi <manmohandwivedi@yahoo.com>
2025-02-19 16:15:15 +0530

    Delete src directory

--- src/main/java/net/backend/journalApp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package net.backend.journalApp.repository;
-
-import net.backend.journalApp.model.UserModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface UserRepository extends  MongoRepository<UserModel, Object>{
-    UserModel findByUserName(String user);
-}
